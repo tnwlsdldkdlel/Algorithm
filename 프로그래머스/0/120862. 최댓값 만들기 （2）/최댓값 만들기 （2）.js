34bc7a523b9540451636986ef722dfee63ad29d7
OHsujin <67881808+tnwlsdldkdlel@users.noreply.github.com>
2024-10-01 20:51:13 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,5 @@
+function solution(numbers) {
+    var answer = numbers.sort((a,b) => b- a);
+    
+    return Math.max(numbers[0] * numbers[1], numbers[numbers.length-1] * numbers[numbers.length-2]);
+}
