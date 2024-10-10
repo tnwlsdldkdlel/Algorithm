0cfb0036c7c1dd04a7df1a2d0dac7125cbe8cc31
OHsujin <67881808+tnwlsdldkdlel@users.noreply.github.com>
2024-10-10 22:52:05 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,13 @@
+function solution(id_pw, db) {
+    for (let i = 0; i < db.length; i++) {
+        if (db[i][0] === id_pw[0]) {
+            if (db[i][1] === id_pw[1]) {
+                return "login";
+            } else {
+                return "wrong pw";
+            }
+        }
+    }
+   
+    return "fail";
+}
